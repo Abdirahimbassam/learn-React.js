0e42d4f05190217efc3009d5220b32cb1cd87b99
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-04-25 20:38:34 +0300

    className in jsx

--- Practical-Lessons/Main-Practical/src/App.jsx
+++ Practical-Lessons/Main-Practical/src/App.jsx
@@ -2,7 +2,10 @@
 function App() {
 
     //   first react code jsx
-    return <h1>Hello, From React.js</h1>
+    // return <h1>Hello, From React.js</h1>
+
+    // className VS Class jsx
+    return <h1 className="title">Hello world from react</h1>
 }
 
 export default App;
