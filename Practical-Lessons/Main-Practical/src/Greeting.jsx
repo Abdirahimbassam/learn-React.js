d7571ea4b1e491d5db82e0f24fa21d17f9b4264c
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-04-25 21:23:02 +0300

    add new component file

---
+++ Practical-Lessons/Main-Practical/src/Greeting.jsx
@@ -0,0 +1,12 @@
+
+// default export component
+// const Greeting = ()=> {
+//     return <h1>Hello, React Course</h1>
+// }
+
+// default expert component
+const Greeting = () => {
+    return <h1>Hello, React Course</h1>
+}
+
+export default Greeting;
