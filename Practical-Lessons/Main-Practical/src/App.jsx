816a9090e973f0c577ebf33d7269c614714bcadb
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-04-25 20:53:53 +0300

    htmlFor for jsx

--- Practical-Lessons/Main-Practical/src/App.jsx
+++ Practical-Lessons/Main-Practical/src/App.jsx
@@ -5,7 +5,20 @@ function App() {
     // return <h1>Hello, From React.js</h1>
 
     // className VS Class jsx
-    return <h1 className="title">Hello world from react</h1>
+    // return <h1 className="title">Hello world from react</h1>
+
+    // htmlFor vs. for jsx
+    return (
+
+      <>
+        <label htmlFor="username">Name:</label>
+        <input type="text" id="username"/>
+
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email"/>
+
+      </>
+    )
 }
 
 export default App;
