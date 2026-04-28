32432a5b142e8fdd6e9bc37d22590a635e19e838
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-04-28 16:59:13 +0300

    learing array in state

--- Practical Lessons/Main-Practical/src/ObjectArray.jsx
+++ Practical Lessons/Main-Practical/src/ObjectArray.jsx
@@ -4,18 +4,37 @@ import { useState } from "react";
 
 const ObjectArray = () => {
 
-    const [ user , SetUser ] = useState( { name: "Abdirahim", age: 21, bio: "Full Stack" } );
+    // OBJECT IN USESTATE
+    // const [ user , SetUser ] = useState( { name: "Abdirahim", age: 21, bio: "Full Stack" } );
 
-    const updateAge = () => {
-        SetUser( {...user, age: user.age + 1} )
+    // const updateAge = () => {
+    //     SetUser( {...user, age: user.age + 1} )
+    // }
+
+    // ARRAY IN USESTATE
+
+    const [ items , setItems ] = useState( [ "Apple", "Banana" ] )
+
+    const additem = () => {
+        setItems([...items, "Orange"])
     }
 
     return (
         
         <>
+            {/* OBJECT IN USESTATE  */}
+            {/* <p>Name:{user.name}, Age: {user.age}, Bio: {user.bio} </p>
+            <button onClick={updateAge}>Increase Age</button>   */}
 
-            <p>Name:{user.name}, Age: {user.age}, Bio: {user.bio} </p>
-            <button onClick={updateAge}>Increase Age</button>
+            {/* ARRAY IN USESTATE    */}
+            <ul>
+                 {
+                    items.map(item => (
+                        <li>{item}</li>
+                    ))
+                 }
+                 <button onClick={additem}>Add Orange</button>
+            </ul>
 
         </>
 
