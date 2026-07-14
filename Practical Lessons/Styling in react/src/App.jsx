815aa89c9dcc3642aace42c97ffe87016e206ab9
Abdirahimbassam <abdirahiimbassam@gmail.com>
2026-07-14 12:18:36 +0300

    lear dynamic class name and conditional rendering style

--- Practical Lessons/Styling in react/src/App.jsx
+++ Practical Lessons/Styling in react/src/App.jsx
@@ -1,13 +1,16 @@
 
 import styles from './app.module.css'
 import { Another } from './Another'
+import { useState } from 'react'
 
 export const App = () => {
 
+    const [ active ,  setActive ] = useState(true)
   return (
 
-    <div className={styles.AppContainer}>
-        <h1 className={styles.headerTitle}>Styling In React.js</h1>
+    <div className={`${styles.AppContainer} ${styles.darkMode}`}>
+
+        <h1 className={ active ? styles.activeHeaderTitle : styles.inActiveHeaderTitle }>Hi Mom</h1>
         <p className={styles.bio}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quae!</p>
       
      <Another />
