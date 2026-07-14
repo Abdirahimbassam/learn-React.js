3bb597964af6404d7cb8c3ee25cd8d8baab9035f
Abdirahimbassam <abdirahiimbassam@gmail.com>
2026-07-14 14:09:29 +0300

    Completed Exercise 19

---
+++ Exercise/Exercise 19/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import TodoApp from './TodoApp'
+
+const App = () => {
+
+  return (
+
+    <div>
+
+      <TodoApp />
+      
+    </div>
+  )
+}
+
+export default App
